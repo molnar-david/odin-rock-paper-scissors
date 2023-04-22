65687881d4169f3fa161f1adad04e674a5caea02
Molnár Dávid <d.molnarka@hotmail.com>
2023-04-22 18:27:06 +0200

    Fix playRound function, implement game function
    Add return statement to playRound's unknown hand played branch

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const HAND_CHOICES = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getPlayerChoice() {
     return prompt("Play your hand!").toLowerCase();
@@ -9,20 +11,33 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
     if (HAND_CHOICES.includes(playerSelection)) {
-
         if (playerSelection === computerSelection) {
             return "Tie!";
         } else if((HAND_CHOICES.indexOf(playerSelection) + 2) % 3 === HAND_CHOICES.indexOf(computerSelection)) {
-            return  `You win! ${playerSelection} beats ${computerSelection}!`;
+            playerScore++;
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
         } else if((HAND_CHOICES.indexOf(playerSelection) + 1) % 3 === HAND_CHOICES.indexOf(computerSelection)) {
+            computerScore++;
             return `You lose! ${computerSelection} beats ${playerSelection}!`;
         }
     } else {
         // Unknown hand played
-        playRound(getPlayerChoice(), getComputerChoice());
+        return playRound(getPlayerChoice(), getComputerChoice());
     }
 }
 
-alert(playRound(getPlayerChoice(), getComputerChoice()));
+function game(rounds) {
+    for(let i=0; i<rounds; i++) {
+        console.log(playRound(getPlayerChoice(), getComputerChoice()));
+    }
+    if (playerScore === computerScore) {
+        console.log(`Final result: Tie ${playerScore}-${computerScore}!`)
+    } else if(playerScore > computerScore) {
+        console.log(`Final result: You win ${playerScore}-${computerScore}!`)
+    } else {
+        console.log(`Final result: You lose ${playerScore}-${computerScore}!`)
+    }
+}
+
+game(5);
