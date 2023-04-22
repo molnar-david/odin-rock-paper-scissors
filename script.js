6a141864f51e8ab1493a4d7a7641f36073110574
Molnár Dávid <d.molnarka@hotmail.com>
2023-04-22 18:00:16 +0200

    Implement playRound function

--- script.js
+++ script.js
@@ -8,4 +8,23 @@ function getComputerChoice() {
     return HAND_CHOICES[Math.floor(Math.random() * 3)];
 }
 
-console.log(getPlayerChoice());
+function playRound(playerSelection, computerSelection) {
+    console.log(`You play: ${playerSelection}`);
+
+    if (HAND_CHOICES.includes(playerSelection)) {
+        console.log(`Computer plays: ${computerSelection}`);
+
+        if (playerSelection === computerSelection) {
+            console.log("Tie!");
+        } else if((HAND_CHOICES.indexOf(playerSelection) + 2) % 3 === HAND_CHOICES.indexOf(computerSelection)) {
+            console.log("You win!");
+        } else if((HAND_CHOICES.indexOf(playerSelection) + 1) % 3 === HAND_CHOICES.indexOf(computerSelection)) {
+            console.log("You lose!");
+        }
+    } else {
+        console.log("Unknown hand...");
+        playRound(getPlayerChoice(), getComputerChoice());
+    }
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
